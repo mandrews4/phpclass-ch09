f69a0cb90dd72a825a1467049a9c4fa8dbd76c3b
mandrews4 <mark.thomas.andrews@gmail.com>
2013-03-29 09:23:39 -0300

    Create logout.php

---
+++ logout.php
@@ -0,0 +1,14 @@
+<?php // Script 9.8 - logout.php
+session_name("MYSESSION");
+session_start();
+$_SESSION = array();
+session_destroy();
+define('TITLE', 'Logout');
+include('templates/header.html');
+?>
+<h2>Welcome to the J.D. Salinger
+Fan Club!</h2>
+<p>You are now logged out.</p>
+<p>Thank you for using this site. We hope that you liked it.<br />
+Blah, blah, blah... Blah, blah, blah...</p>
+<?php include('templates/footer.html'); ?>
